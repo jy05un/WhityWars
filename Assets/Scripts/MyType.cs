0c3ba42c8f65b825baff8c1d7e1a2be4b200c7cf
jyosun <jys000305@naver.com>
2021-05-08 11:01:21 +0900

    upload files

---
+++ Assets/Scripts/MyType.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class MyType : MonoBehaviour
+{
+    public enum Unit { Sword, Range, Guard, Wizard, Bullet };
+    public enum Sound { Victory, Defeat, Buy, Upgrade, Sword, Range, RangeHit, Guard, Magic };
+}
